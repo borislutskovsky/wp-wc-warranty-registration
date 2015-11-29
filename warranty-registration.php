687c759222a70fcd2c061f1053c06165433bf2d8
borislutskovsky@gmail.com <borislutskovsky@gmail.com>
2015-11-29 11:46:14 -0800

    Case:#2160 - initial import

---
+++ warranty-registration.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: WooCommerce Warranty Registration
+ * Description: a plugin that allows users to register their warranty for products
+ * Version: 1.0
+ * Author: Boris Lutskovsky
+ * Author URI: http://www.iamboris.com
+ * License: MIT
+ */
